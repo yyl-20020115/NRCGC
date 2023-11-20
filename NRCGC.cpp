2593c63a84eb007becfefd0441bb2ed49d7b2a66
Yilin Yang <yyl_20050115@hotmail.com>
2023-11-20 19:33:25 +0800

    fx

--- NRCGC.cpp
+++ NRCGC.cpp
@@ -1,13 +1,63 @@
 ﻿#include "NRCGC.h"
 
-int main()
-{
+ReferenceRingBuffer* buffer = nullptr;
 
-	auto* buffer = ReferenceRingBuffer::Create(256,8);
+bool testing = true;
+bool collecting = true;
+
+static void collector_function() {
 	if (buffer != nullptr) {
+		while (collecting) {
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			buffer->TryCollectOnce();
+		}
+	}
+}
+static void tester_function() {
+	if (buffer != nullptr) {
+		while (testing) {
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			intptr_t handle = buffer->Allocate(256);
+			if (handle != ~0) {
+				std::this_thread::sleep_for(std::chrono::seconds(1));
+				buffer->Release(handle);
+			}
+		}
+	}
+}
+const int MaxTesters = 4;
+const int MaxCollectors = 4;
+std::vector<std::thread*> threads;
 
-		size_t handle = buffer->Allocate(256);
-		buffer->Release(handle);
+int main(int argc, char* argv[])
+{
+	buffer = ReferenceRingBuffer::Create(256,8);
+	if (buffer != nullptr) {
+		//intptr_t handle = buffer->Allocate(256);
+		//if (handle != ~0) {
+		//	buffer->Release(handle);
+		//}
+		for (int i = 0; i < MaxCollectors; i++) {
+			std::thread* thread = new std::thread(collector_function);
+			threads.push_back(thread);
+		}
+		for (int i = 0; i < MaxTesters; i++) {
+			std::thread* thread = new std::thread(tester_function);
+			threads.push_back(thread);
+		}
+		testing = true;
+		collecting = true;
+
+		std::this_thread::sleep_for(std::chrono::seconds(1000));
+
+		testing = false;
+		collecting = false;
+
+		for (auto& thread : threads) {
+			thread->join();
+			delete thread;
+		}
+		threads.clear();
 
 		delete buffer;
 	}
--- NRCGC.h
+++ NRCGC.h
@@ -19,56 +19,67 @@
 template<typename T>
 class RingBuffer {
 public:
-    static const int DefaultIdleCount = 8;
+    static const intptr_t DefaultIdleCount = 8;
 public:
-    static RingBuffer<T>* Create(size_t count, int idle_count = DefaultIdleCount) {
+    static RingBuffer<T>* Create(intptr_t count, intptr_t idle_count = DefaultIdleCount) {
         unsigned char* address = (unsigned char*)
-            malloc(sizeof(RingBuffer<T>) + count * sizeof(T) + count * sizeof(int));
+            malloc(sizeof(RingBuffer<T>) + count * sizeof(T) + count * sizeof(intptr_t));
         if (address != nullptr) {
             address = (unsigned char*)(new ((RingBuffer<T>*)address) RingBuffer<T>(address, count, idle_count));
         }
         return (RingBuffer<T>*)address;
     }
-
 public:
-    RingBuffer(unsigned char* address, size_t count, int idle_count = DefaultIdleCount)
-        : pbuffer(0)
+    RingBuffer(unsigned char* address, intptr_t count, intptr_t idle_count = DefaultIdleCount)
+        : used(0)
+        , pbuffer(0)
         , pflags_(0)
         , idle_count(idle_count)
-        , capacity_mask(~count)
+        , capacity_mask(count-1)
         , send_index()
-        , receive_index() {
-        // 确保size是2的整数次幂
-        assert((count & (count - 1)) == 0 && "Size must be a power of 2");
+        , receive_index() 
+
+    {
+        // 确保count是2的整数次幂
+        assert((count & (count - 1)) == 0 && "count must be a power of 2");
         memset(address + (this->pbuffer = sizeof(RingBuffer<T>)), 0, count * sizeof(T));
-        memset(address + (this->pflags_ = sizeof(RingBuffer<T>) + count * sizeof(T)), 0, count * sizeof(int));
+        memset(address + (this->pflags_ = sizeof(RingBuffer<T>) + count * sizeof(T)), 0, count * sizeof(intptr_t));
     }
 
-    ~RingBuffer() {}
+    ~RingBuffer(){}
 
 public:
     // 发送数据到缓冲区
-    size_t Send(const T& value) {
+    intptr_t Send(const T& value) {
         // 原子操作当前的发送位置，保证不管多个进程还是多个线程都在缓存中找到唯一位置
-        size_t i = send_index.fetch_add(1, std::memory_order_seq_cst);
-        ((T*)((unsigned int*)this + pbuffer))[i & this->capacity_mask] = value;
+        intptr_t i = send_index.fetch_add(1, std::memory_order_seq_cst);
+        ((T*)((unsigned char*)this + this->pbuffer))[i & this->capacity_mask] = value;
         //原子操作将全1写入pflags[i]
 #ifdef _WIN32
-        InterlockedOr((long*)&((int*)this + this->pflags_)[i & capacity_mask], 0xffffffff);
+#ifdef _WIN64
+        _interlockedor64((intptr_t*)&((unsigned char*)this + this->pflags_)[i & capacity_mask], ~0LL);
 #else
-        __sync_lock_test_and_set(&((int*)this + this->pflags_)[i & capacity_mask], 0xffffffff);
+        _interlockedOr((intptr_t*)&((unsigned char*)this + this->pflags_)[i & capacity_mask], ~0);
+#endif
+#else
+        __sync_lock_test_and_set(&((unsigned char*)this + this->pflags_)[i & capacity_mask], ~0);
 #endif
         return i;
     }
 
+public:
     // 等待并从缓冲区中获取数据
-    void Wait(T& value) {
+    intptr_t Wait(T& value) {
         int ic = 0;
-        size_t ir = receive_index.load(std::memory_order_seq_cst);
+        intptr_t ir = receive_index.load(std::memory_order_seq_cst);
 #ifdef _WIN32
-        while (0!=_InterlockedCompareExchange(&((unsigned long*)this + pflags_)[ir & this->capacity_mask], 0xffffffff, 0))
+#ifdef _WIN64
+        while (0 != _InterlockedCompareExchange64(&(((volatile __int64*)((unsigned char*)this + pflags_))[ir & this->capacity_mask]), ~0, 0))
 #else
-        while (!__sync_bool_compare_and_swap(&((unsigned int*)this + pflags_)[ir & this->capacity_mask], 0xffffffff, 0))
+        while (0 != _InterlockedCompareExchange(&((unsigned char*)this + pflags_)[ir & this->capacity_mask], ~0, 0))
+#endif
+#else
+        while (!__sync_bool_compare_and_swap(&((unsigned char*)this + pflags_)[ir & this->capacity_mask], ~0, 0))
 #endif   
         {
             ir++;
@@ -85,32 +96,51 @@ public:
         ir &= this->capacity_mask;
         receive_index.store(ir, std::memory_order_seq_cst);
         value = ((T*)((unsigned char*)this + this->pbuffer))[ir];
+        return ir;
     }
 
 protected:
-    size_t pbuffer;
-    size_t pflags_;
-    int idle_count;
-    std::atomic_size_t send_index;
-    std::atomic_size_t receive_index;
-    const size_t capacity_mask;
+    intptr_t used;
+    intptr_t pbuffer;
+    intptr_t pflags_;
+    intptr_t idle_count;
+    std::atomic_intptr_t send_index;
+    std::atomic_intptr_t receive_index;
+    const intptr_t capacity_mask;
 };
 
 struct Reference {
+public:
     void* Ptr = nullptr;
     intptr_t _Size = 0;
     intptr_t Count = 0;
+public:
+    Reference(
+        void* Ptr = nullptr, 
+        intptr_t _Size = 0, 
+        intptr_t Count = 1) 
+    {
+        this->Ptr = Ptr;
+        this->_Size = _Size;
+        this->Count = Count;
+    }
+
+    void Dispose() {
+        if (this->Ptr != nullptr) {
+            free(this->Ptr);
+            this->Ptr = nullptr;
+        }
+        this->Count = 0;
+        this->_Size = 0;
+    }
 };
 
 class ReferenceRingBuffer 
-    : public RingBuffer<Reference>
-{
+    : public RingBuffer<Reference> {
 public:
-    static ReferenceRingBuffer* Create(size_t count, int idle_count = DefaultIdleCount)
-    {
+    static ReferenceRingBuffer* Create(intptr_t count, intptr_t idle_count = DefaultIdleCount) {
         unsigned char* address = (unsigned char*)
-            malloc(sizeof(ReferenceRingBuffer) + count * sizeof(Reference) + count * sizeof(unsigned char));
-
+            malloc(sizeof(ReferenceRingBuffer) + count * sizeof(Reference) + count * sizeof(intptr_t));
         if (address != nullptr) {
             address = (unsigned char*)(new ((ReferenceRingBuffer*)address) 
                 ReferenceRingBuffer(address, count, idle_count));
@@ -118,43 +148,70 @@ public:
         return (ReferenceRingBuffer*)address;
     }
 public:
-    ReferenceRingBuffer(unsigned char* address, size_t count, int idle_count = DefaultIdleCount)
+    ReferenceRingBuffer(unsigned char* address, intptr_t count, intptr_t idle_count = DefaultIdleCount)
         : RingBuffer<Reference>(address,count,idle_count){}
-
 public:
-    size_t Allocate(size_t size) {
+    intptr_t Allocate(intptr_t size) {
+        if (this->used > this->capacity_mask) return -1LL;
         void* ptr = malloc(size);
         if (ptr != nullptr) {
-            Reference reference = { ptr,(intptr_t)size,1 };
-            return this->Send(reference);
+            intptr_t p = this->Send(Reference(ptr, size));
+            if (p != -1LL) {
+#ifdef _WIN32
+#ifdef _WIN64
+                _interlockedincrement64(&this->used);
+#else
+                _interlockedincrement(&this->used);
+#endif
+#else
+                __sync_add_and_fetch(&this->used, -1);
+#endif
+                return p;
+            }
         }
-        return ~0ULL;
+        return -1LL;
     }
+
 public:
-    size_t Release(size_t handle) {
-        Reference& reference =
-            ((Reference*)((unsigned char*)this + this->pbuffer))[handle & this->capacity_mask];
+    intptr_t Release(intptr_t handle) {
+        Reference& reference = ((Reference*)((unsigned char*)this + this->pbuffer))[handle & this->capacity_mask];
+        if (reference.Count == 0) {
+            return -1LL;
+        }
+
 #ifdef _WIN32
 #ifdef _WIN64
         intptr_t count = _interlockeddecrement64(&reference.Count);
+        _interlockeddecrement64(&this->used);
 #else
         intptr_t count = _interlockeddecrement(&reference.Count);
+        _interlockeddecrement(&this->used);
 #endif
 #else
         intptr_t count = __sync_add_and_fetch(&reference.Count, -1);
+        __sync_add_and_fetch(&this->used, -1);
 #endif
-        if (count == 0) {
-            free(reference.Ptr);
-            reference.Ptr = nullptr;
-            reference.Count = 0;
-            reference._Size = 0;
+        return count;
+    }
+
+public:
+    intptr_t TryCollectOnce() {
+        Reference reference;
+        intptr_t handle = this->Wait(reference);        
+        if (reference.Count == 0) {
+            reference.Dispose();
 #ifdef _WIN32
-            _InterlockedAnd((long*)&((int*)this + this->pflags_)[handle & this->capacity_mask], 0);
+#ifdef _WIN64
+            InterlockedAdd64((intptr_t*)&((unsigned char*)this + this->pflags_)[handle & this->capacity_mask], 0);
 #else
-            __sync_lock_test_and_set(&((long*)this + this->pflags_)[i & this->capacity_mask], 0);
+            InterlockedAdd((intptr_t*)&((unsigned char*)this + this->pflags_)[handle & this->capacity_mask], 0);
 #endif
+#else
+            __sync_lock_test_and_set(&((unsigned char*)this + this->pflags_)[i & this->capacity_mask], 0);
+#endif
+            return handle;
         }
-        return (size_t)count;
+        return -1LL;
     }
 };
 
